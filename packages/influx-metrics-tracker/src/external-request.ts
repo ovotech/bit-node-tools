e73496c5b838df84b78864bd155ca2f7334359a9
hayfield <hayden.field@ovoenergy.com>
2019-06-24 14:52:53 +0100

    Add tracker for external requests
    Provides a common way to track the response times of external services.

---
+++ packages/influx-metrics-tracker/src/external-request.ts
@@ -0,0 +1,13 @@
+import { MetricsTracker } from '@ovotech/influx-metrics-tracker';
+
+export class ExternalRequestMetricsTracker extends MetricsTracker {
+  private static externalRequestTimeMeasurementName = 'external-request-time';
+
+  async trackRequestTime(externalServiceName: string, requestName: string, timeMs: number, statusCode?: number) {
+    await this.trackPoint(
+      ExternalRequestMetricsTracker.externalRequestTimeMeasurementName,
+      { requestName, externalServiceName, status: statusCode ? statusCode.toString() : '' },
+      { timeMs: Math.round(timeMs), count: 1 },
+    );
+  }
+}
