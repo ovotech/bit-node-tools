654bfdbb60a5a62b673549dcf3257068c33695d1
hayfield <hayden.field@ovoenergy.com>
2019-08-20 15:22:16 +0100

    Remove interim function

--- packages/keycloak-auth/src/index.ts
+++ packages/keycloak-auth/src/index.ts
@@ -1,5 +1,5 @@
 export { KeycloakAuthError } from './KeycloakAuthError';
 export { login, refresh, authenticate, AuthResponse, isExpired, KeycloakResponse } from './api';
 export { KeycloakAuth, KeycloakAuthOptions } from './KeycloakAuth';
-export { keycloakAxios, keycloakAxiosInterceptor, KeycloakAxiosOptions } from './keycloakAxios';
+export { keycloakAxios, KeycloakAxiosOptions } from './keycloakAxios';
 export { decodeAccessToken, BouncerConfig } from './KeycloakBouncer';
