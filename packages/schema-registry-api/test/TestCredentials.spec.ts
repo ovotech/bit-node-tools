61d2e517400c47a19fd6c1265783f6d353efe6bf
James Tarling <james.tarling@ovoenergy.com>
2021-09-09 12:49:16 +0100

    Fix integration test

--- packages/schema-registry-api/test/TestCredentials.spec.ts
+++ packages/schema-registry-api/test/TestCredentials.spec.ts
@@ -1,5 +1,9 @@
 import { Schema } from 'avsc';
 import { addSubjectVersion } from '../src';
+import fetch from 'node-fetch';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('node-fetch');
 
 const schema1: Schema = {
   type: 'record',
@@ -9,7 +13,9 @@ const schema1: Schema = {
 
 describe('Unit test', () => {
   it('Test credentials leakage', async () => {
-    const url = 'http://user:pass@something.com/';
+    const mockFetch = mocked(fetch);
+    mockFetch.mockRejectedValue(new Error('Error calling http://user:pass@something.com'));
+    const url = 'foo';
     try {
       await addSubjectVersion(url, '', schema1);
     } catch (error) {
