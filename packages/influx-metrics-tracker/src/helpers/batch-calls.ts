bdd3c8c79c711d2b091bf8a09ba45f0328181515
Blair Calderwood <blair.calderwood@radically.digital>
2022-03-21 19:07:17 +0000

    Added singleton(ish) ability to batchCalls class

--- packages/influx-metrics-tracker/src/helpers/batch-calls.ts
+++ packages/influx-metrics-tracker/src/helpers/batch-calls.ts
@@ -1,11 +1,11 @@
 import { Logger } from '@ovotech/winston-logger';
 
-export default class BatchCalls {
+class BatchCalls {
   private batchData: unknown[];
 
   constructor(
     protected batchSendIntervalMs: number,
-    private batchCall: (...args: any[]) => void,
+    private callback: (...args: any[]) => void,
     protected logger: Logger,
   ) {
     this.batchData = [];
@@ -19,7 +19,7 @@ export default class BatchCalls {
   private async sendBatches(batchData: unknown[]) {
     if (batchData.length > 0) {
       try {
-        return this.batchCall(batchData);
+        return this.callback(batchData);
       } catch (error) {
         this.logger.error('Error sending batch call to external service', {
           error: error && error.message ? error.message : 'Unknown error',
@@ -39,4 +39,38 @@ export default class BatchCalls {
       await this.sendBatches(tempBatchData);
     }, batchSendIntervalMs);
   }
+}
+
+interface Instance {
+  classInstance: BatchCalls;
+  batchSendIntervalMs: number;
+  callback: (...args: any[]) => void;
+  logger: Logger;
+}
+
+const currentInstances: Instance[] = [];
+
+export default function getBatchCallsInstance(
+  batchSendIntervalMs: number,
+  callback: (...args: any[]) => void,
+  logger: Logger,
+): BatchCalls {
+  let foundInstance = currentInstances.find(
+    currentInstance =>
+      currentInstance.batchSendIntervalMs === batchSendIntervalMs &&
+      currentInstance.callback === callback &&
+      currentInstance.logger === logger,
+  );
+
+  if (!foundInstance) {
+    foundInstance = {
+      classInstance: new BatchCalls(batchSendIntervalMs, callback, logger),
+      batchSendIntervalMs,
+      callback,
+      logger,
+    };
+    currentInstances.push(foundInstance);
+  }
+
+  return foundInstance.classInstance;
 }
